0e6a2d3e51bd491cbfc20fcf88497bf320829565
areeb <mohammedareebali740@gmail.com>
2024-03-24 11:50:59 +0000

    fix : try

--- src/seachpage/Marker.tsx
+++ src/seachpage/Marker.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import { Marker, Popup } from "react-map-gl";
 interface MapMarkerProps {
@@ -42,49 +41,4 @@ const MapMarker: React.FC<MapMarkerProps> = ({
       </Popup>
     </Marker>
   );
-=======
-import React from "react";
-import { Marker, Popup } from "react-map-gl";
-interface MapMarkerProps {
-    latitude: number;
-    longitude: number;
-    campground: Campground;
-    onClick: () => any;
-  }
-  interface Campground {
-    
-    _id: string;
-    name: string;
-    location: string;
-    description: string;
-    imageUrl: string;
-    __v: number;
-  }
-  
-const MapMarker: React.FC<MapMarkerProps> = ({
-    latitude,
-    longitude,
-    campground,
-    onClick,
-  }) => (
-    <Marker latitude={latitude} longitude={longitude}>
-      <button className="marker-btn" onClick={onClick}>
-        
-      </button>
-      <Popup
-        latitude={latitude}
-        longitude={longitude}
-        onClose={() => {}}
-        closeButton={false}
-        anchor="left"
-      >
-        <div>
-          <h2>{campground.name}</h2>
-          <p>{campground.location}</p>
-          <p>{campground.description}</p>
-        </div>
-      </Popup>
-    </Marker>
-  );
->>>>>>> 1ebec6a (fix:setup with backend)
   export default MapMarker;
--- src/seachpage/ReviewDisplay.tsx
+++ src/seachpage/ReviewDisplay.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import { Accordion } from "react-bootstrap";
 
@@ -46,52 +45,3 @@ const ReviewDisplay: React.FC<Props> = ({ reviews }) => {
 };
 
 export default ReviewDisplay;
-=======
-import React from "react";
-import { Accordion } from "react-bootstrap";
-
-interface Review {
-  emojiRating: number;
-  comment: string;
-  postedBy: string;
-  createdAt: string;
-}
-
-interface Props {
-  reviews: Review[];
-}
-
-const getEmoji = (rating: number): string => {
-  const emojis = ["😡", "😞", "😐", "😃", "😍"];
-
-  if (rating < 1 || rating > 5) return "❓";
-
-  return emojis[rating - 1];
-};
-
-const formatDate = (dateString: string): string => {
-  const date = new Date(dateString);
-  return date.toLocaleDateString("en-US", {
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  });
-};
-
-const ReviewDisplay: React.FC<Props> = ({ reviews }) => {
-  return (
-    <Accordion defaultActiveKey="0">
-      {reviews.map((review, index) => (
-        <Accordion.Item key={index} eventKey={index.toString()}>
-          <Accordion.Header>
-            {getEmoji(review.emojiRating)} - Posted by {review.postedBy} on {formatDate(review.createdAt)}
-          </Accordion.Header>
-          <Accordion.Body>{review.comment}</Accordion.Body>
-        </Accordion.Item>
-      ))}
-    </Accordion>
-  );
-};
-
-export default ReviewDisplay;
->>>>>>> 1ebec6a (fix:setup with backend)
